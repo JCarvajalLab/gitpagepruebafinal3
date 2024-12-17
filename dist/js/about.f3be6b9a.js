f37f4f1ea0d11a214d10e19d609bcff5c6d7509d
JCarvajalLab <carvajal.jordan94@gmail.com>
2024-12-17 13:30:31 -0300

    Se agregan vuetify al proyecto

--- dist/js/about.f3be6b9a.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkgitpagepruebafinal3"]=self["webpackChunkgitpagepruebafinal3"]||[]).push([[594],{603:function(a,n,u){u.r(n),u.d(n,{default:function(){return f}});var e=u(6768);const t={class:"about"};function r(a,n){return(0,e.uX)(),(0,e.CE)("div",t,n[0]||(n[0]=[(0,e.Lk)("h1",null,"This is an about page",-1)]))}var i=u(1241);const s={},c=(0,i.A)(s,[["render",r]]);var f=c}}]);
-//# sourceMappingURL=about.f3be6b9a.js.map
